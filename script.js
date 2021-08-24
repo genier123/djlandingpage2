a2283d685c6fa3b6c885e9ba9c22eb07554ff318
Patrick Genier <patrick.j.genier@gmail.com>
2021-08-24 14:01:27 -0400

    8-24-21-1stCom

---
+++ script.js
@@ -0,0 +1,21 @@
+let FormName = document.querySelector('Name');
+let FormEmail = document.querySelector('Email');
+let FormMessage = document.querySelector('Message');
+
+function sendEmail(){
+
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "djwatelse",
+        Password : "Monday5678",
+        To : 'djwatelse@gmail.com',
+        Name: 'Name',
+        From : "Email",
+        Body : "Message"
+    }).then(
+      message => alert(message)
+    );
+    
+} 
+
+/*e8603e92-b0d1-494c-bc9d-676d4088abbb*/
