37054be4b4f4d0e5b1dd20779bf8b8b95312c0e6
Patrick Genier <patrick.j.genier@gmail.com>
2021-08-24 14:23:49 -0400

    8-24-21-4thCom

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-let FormName = document.querySelector('Name');
-let FormEmail = document.querySelector('Email');
-let FormMessage = document.querySelector('Message');
+//let FormName = document.querySelector('Name');
+//let FormEmail = document.querySelector('Email');
+//let FormMessage = document.querySelector('Message');
 
 function sendEmail(){
 
     Email.send({
-        Host : "smtp.gmail.com",
-        Username : "djwatelse",
-        Password : "Monday5678",
+        //Host : "smtp.gmail.com",
+        //Username : "djwatelse",
+        //Password : "Monday5678",
+        SecureToken: 'e8603e92-b0d1-494c-bc9d-676d4088abbb',
         To : 'djwatelse@gmail.com',
         Name: 'Name',
         From : "Email",
         Body : "Message"
     }).then(
-      message => alert("Your Message Has Been Sent Someone Will get back to ASAP")
+      message => alert("Your Message Has Been Sent Someone Will get back to ASAP"))
     );
     
 } 
