80f3cbb106feb8fa5841790afbfebab38fb9ac2f
Patrick Genier <patrick.j.genier@gmail.com>
2021-08-24 14:42:19 -0400

    8-24-21-7thCom

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function sendEmail(){
         //Host : "smtp.gmail.com",
         //Username : "djwatelse",
         //Password : "Monday5678",
-        SecureToken: 'e8603e92-b0d1-494c-bc9d-676d4088abbb',
+        SecureToken: '47b1a2ef-1808-4569-bd17-944a4559a55a',
         To : 'djwatelse@gmail.com',
         From : "you@isp.com",
         Subject : "This is the subject",
